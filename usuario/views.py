3c82e54e928a53648cb59a0fd39cce62ed6be5cb
unknown <evallardy@gmail.com>
2023-12-21 17:45:22 -0600

    formato elabora 1

--- usuario/views.py
+++ usuario/views.py
@@ -11,7 +11,7 @@ def cambia_contrasena(request):
         if form.is_valid():
             user = form.save()
             update_session_auth_hash(request, user)  # importante para mantener la sesión del usuario activa
-            return redirect('index')  # cambia esto por la url a la que quieres redirigir al usuario después de actualizar la contraseña
+            return redirect('elabora')  # cambia esto por la url a la que quieres redirigir al usuario después de actualizar la contraseña
     else:
         form = PasswordChangeForm(request.user)
     return render(request, 'registration/cambia_contrasena.html', {'form': form})
